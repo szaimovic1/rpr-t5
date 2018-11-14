2d07761436ef78f95c921cc58d0a3dac83359953
szaimovic1 <szaimovic1@etf.unsa.ba>
2018-11-14 15:27:52 +0100

    skontala kako funkcionise css. sta je sa infinity i podešavanjem dizajna, tj. jesam li koristila prave opcije? kod u prosesu pisanja...

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,33 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public Label calField;
+    public Button btnRem;
+    public Button btnDiv;
+    public Button btnMull;
+    public Button btnSub;
+    public Button btnAdd;
+    public Button btnEqu;
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btn4;
+    public Button btn5;
+    public Button btn6;
+    public Button btn7;
+    public Button btn8;
+    public Button btn9;
+    public Button btn0;
+    public Button btnMark;
+
+    public void calculateClick(ActionEvent actionEvent) {
+        //System.out.println("Login glasi: " + loginField.getText());
+        //prijavaBtn.setText(loginField.getText());
+    }
 
 }
